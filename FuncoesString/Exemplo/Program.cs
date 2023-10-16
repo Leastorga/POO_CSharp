6ef17b14c0ae01d53c643590ef286586572432cb
Leastorga <leticia.trancozo@gmail.com>
2023-10-16 10:08:29 -0300

    Utilizando Substring

--- FuncoesString/Exemplo/Program.cs
+++ FuncoesString/Exemplo/Program.cs
@@ -9,11 +9,11 @@
             string s1 = original.ToUpper();
             string s2 = original.ToLower();
             string s3 = original.Trim(); // Apagar os espaços depois ou antes da string
-            
+
             int n1 = original.IndexOf("bc"); // Procurar pela primeira ocorrência do bc
             int n2 = original.LastIndexOf("bc"); // Procurar pela última ocorrência do bc
 
-
+            string s4 = original.Substring(3); // recorta o que estiver antes
 
             Console.WriteLine("Original: -" + original + "-");
             Console.WriteLine("ToUpper: -" + s1 + "-");
@@ -21,6 +21,8 @@
             Console.WriteLine("ToTrim: -" + s3 + "-");
             Console.WriteLine("IndexOf('bc'):" + n1) ;
             Console.WriteLine("LastIndexOf('bc'):" + n2) ;
+
+            Console.WriteLine("Subtring: "+ s4);
         }
     }
 }
