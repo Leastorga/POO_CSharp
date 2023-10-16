ed89616b254ddd388f0f388d0e5dfb4a1ea7d7f2
Leastorga <leticia.trancozo@gmail.com>
2023-10-16 12:11:21 -0300

    Utulizando o datetime

--- DateTime/Exemplo/Program.cs
+++ DateTime/Exemplo/Program.cs
@@ -4,8 +4,16 @@
     {
         public static void Main(string[] args)
         {
-            DateTime d1 = DateTime.Now;
+            DateTime d2 = new DateTime(2018,11,25); // Especificar a data
+            Console.WriteLine(d2);
 
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3); // Especificar a data e o horário
+            Console.WriteLine(d3);
+
+            DateTime d4 = new DateTime(2018, 11, 25, 20, 45, 3, 500); // Especificar o milisegundo, porém na hora da impressão a formatação não considera o milisegundo.
+            Console.WriteLine(d4);
+
+            DateTime d1 = DateTime.Now;
             Console.WriteLine(d1);
         }
 
