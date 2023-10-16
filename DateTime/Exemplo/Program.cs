75ed36e4d0ed1b949938ab0f61afcc3569aecca6
Leastorga <leticia.trancozo@gmail.com>
2023-10-16 12:58:52 -0300

    Utilizando datetime today, utcnow

--- DateTime/Exemplo/Program.cs
+++ DateTime/Exemplo/Program.cs
@@ -4,17 +4,23 @@
     {
         public static void Main(string[] args)
         {
-            DateTime d2 = new DateTime(2018,11,25); // Especificar a data
+            DateTime d1 = new DateTime(2018,11,25); // Especificar a data
+            Console.WriteLine(d1);
+
+            DateTime d2 = new DateTime(2018, 11, 25, 20, 45, 3); // Especificar a data e o horário
             Console.WriteLine(d2);
 
-            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3); // Especificar a data e o horário
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3, 500); // Especificar o milisegundo, porém na hora da impressão a formatação não considera o milisegundo.
             Console.WriteLine(d3);
 
-            DateTime d4 = new DateTime(2018, 11, 25, 20, 45, 3, 500); // Especificar o milisegundo, porém na hora da impressão a formatação não considera o milisegundo.
+            DateTime d4 = DateTime.Now;
             Console.WriteLine(d4);
 
-            DateTime d1 = DateTime.Now;
-            Console.WriteLine(d1);
+            DateTime d5 = DateTime.Today; // Data de hoje sem o horário
+            Console.WriteLine(d5);
+
+            DateTime d6 = DateTime.UtcNow; // Horário do meridiano de Greenwich
+            Console.WriteLine(d6);
         }
 
     }
