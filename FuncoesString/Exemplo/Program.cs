c08e2b3590e17d2f50d1710eda5163c43176127c
Leastorga <leticia.trancozo@gmail.com>
2023-10-16 10:01:49 -0300

    Uso do Last index of

--- FuncoesString/Exemplo/Program.cs
+++ FuncoesString/Exemplo/Program.cs
@@ -9,11 +9,17 @@
             string s1 = original.ToUpper();
             string s2 = original.ToLower();
             string s3 = original.Trim(); // Apagar os espaços depois ou antes da string
+            int n1 = original.IndexOf("bc"); // Procurar pela primeira ocorrência do bc
+            int n2 = original.LastIndexOf("bc"); // Procurar pela última ocorrência do bc
+
+
 
             Console.WriteLine("Original: -" + original + "-");
             Console.WriteLine("ToUpper: -" + s1 + "-");
             Console.WriteLine("ToLower: -" + s2 + "-");
             Console.WriteLine("ToTrim: -" + s3 + "-");
+            Console.WriteLine("IndexOf('bc'):" + n1) ;
+            Console.WriteLine("LastIndexOf('bc'):" + n2) ;
         }
     }
 }
