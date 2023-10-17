a9c4a34b65192b22bc00766cf947573f9919bee4
Leastorga <leticia.trancozo@gmail.com>
2023-10-17 08:58:51 -0300

    Utilizando o from days

--- TimeSpan/Exemplo/Program.cs
+++ TimeSpan/Exemplo/Program.cs
@@ -9,12 +9,13 @@
             TimeSpan t3 = new TimeSpan(1,2,11,21);
             TimeSpan t4 = new TimeSpan(1,2,11,21,321);
 
-
-
             Console.WriteLine(t1);
             Console.WriteLine(t2);
             Console.WriteLine(t3);
             Console.WriteLine(t4);
+
+            TimeSpan t5 = TimeSpan.FromDays(1.5); // Dá a duração de um dia e meio 
+            Console.WriteLine(t5);
             
         }
     }
