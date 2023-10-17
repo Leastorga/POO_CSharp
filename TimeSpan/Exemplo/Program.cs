05eb1f9dcc079c55d3ec7480f13e0b08b2d64430
Leastorga <leticia.trancozo@gmail.com>
2023-10-17 09:02:55 -0300

    Utilizando os Froms

--- TimeSpan/Exemplo/Program.cs
+++ TimeSpan/Exemplo/Program.cs
@@ -14,8 +14,19 @@
             Console.WriteLine(t3);
             Console.WriteLine(t4);
 
-            TimeSpan t5 = TimeSpan.FromDays(1.5); // Dá a duração de um dia e meio 
+            TimeSpan t5 = TimeSpan.FromDays(1.5); // Dá a duração de um dia e meio
+            TimeSpan t6 = TimeSpan.FromHours(1.5);
+            TimeSpan t7 = TimeSpan.FromMinutes(1.5);
+            TimeSpan t8 = TimeSpan.FromSeconds(1.5);
+            TimeSpan t9 = TimeSpan.FromMilliseconds(1.5);
+            TimeSpan t10 = TimeSpan.FromTicks(900000000L);
+
             Console.WriteLine(t5);
+            Console.WriteLine(t6);
+            Console.WriteLine(t7);
+            Console.WriteLine(t8);
+            Console.WriteLine(t9);
+            Console.WriteLine(t10);
             
         }
     }
