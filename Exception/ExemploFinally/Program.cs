9a118246725968d6dd0bcfa0349e85f4a500613a
Leastorga <leticia.trancozo@gmail.com>
2023-10-30 13:25:31 -0300

    Aprendendo sobre Finally

---
+++ Exception/ExemploFinally/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExemploFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            // Roda independente se houver exceção ou não
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+
+            }
+        }
+    }
+}
