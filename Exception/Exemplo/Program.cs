6ae5efc9b154bcef04bd0a887602c7322198a4ae
Leastorga <leticia.trancozo@gmail.com>
2023-10-30 10:41:32 -0300

    Testando o tratamento de exceções

---
+++ Exception/Exemplo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not allowed"); // Message é um atributo no Exception que mostra o erro
+            }
+            catch(FormatException e){
+                Console.WriteLine($"Format error! {e}");
+                
+            }
+
+        }
+    }
+}
