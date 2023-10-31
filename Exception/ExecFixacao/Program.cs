914793fc9aa51ad1fda46c26eaa8ba68bd8dc4c4
Leastorga <leticia.trancozo@gmail.com>
2023-10-31 10:18:38 -0300

    Desenvolvendo program

---
+++ Exception/ExecFixacao/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using ExecFixacao.Entities;
+using ExecFixacao.Entities.Exception;
+
+namespace ExecFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Enter account data: ");
+
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new(number, holder, balance, withdrawLimit);
+
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(withdraw);
+
+                Console.WriteLine($"New balance: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+            }catch (DomainException e){
+                Console.WriteLine($"Withdraw error: {e.Message}"); 
+            }catch(Exception e){
+                Console.WriteLine($"Uncexpected error: {e.Message}");
+            }
+        }
+    }
+}
