0140d0ce760ad256f320cec6e079702171c8e5f1
Leastorga <leticia.trancozo@gmail.com>
2023-10-31 10:22:34 -0300

    Atualizando program

--- Exception/ExecFixacao/Program.cs
+++ Exception/ExecFixacao/Program.cs
@@ -10,34 +10,38 @@ namespace ExecFixacao
         static void Main(string[] args)
         {
 
+
+            Console.WriteLine("Enter account data: ");
+
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine();
+
+            Console.Write("Enter amount for withdraw: ");
+            double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             try
             {
-                Console.WriteLine("Enter account data: ");
-
-                Console.Write("Number: ");
-                int number = int.Parse(Console.ReadLine());
-
-                Console.Write("Holder: ");
-                string holder = Console.ReadLine();
-
-                Console.Write("Initial balance: ");
-                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                Console.Write("Withdraw limit: ");
-                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                Account account = new(number, holder, balance, withdrawLimit);
-
-                Console.WriteLine();
-
-                Console.Write("Enter amount for withdraw: ");
-                double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 account.Withdraw(withdraw);
-
                 Console.WriteLine($"New balance: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
-            }catch (DomainException e){
-                Console.WriteLine($"Withdraw error: {e.Message}"); 
-            }catch(Exception e){
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Withdraw error: {e.Message}");
+            }
+            catch (Exception e)
+            {
                 Console.WriteLine($"Uncexpected error: {e.Message}");
             }
         }
