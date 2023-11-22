ce29fa94c18ecbec664e5106d43f14e4bd09f425
Leastorga <leticia.trancozo@gmail.com>
2023-11-22 13:50:52 -0300

    Aprendendo utilizar o using

---
+++ Arquivos/Using/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UsandoUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Users\97525\Desktop\POO\Arquivos\file01.txt";
+
+            try
+            {
+                 // Ao utilizarmos o using, assim que terminar de utilizar o arquivo ele será fechado automaticamente. 
+                
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+
+
+            }
+        }
+    }
+}
