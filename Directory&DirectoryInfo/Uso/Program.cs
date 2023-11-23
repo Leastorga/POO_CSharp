2faab678e040fe271f9e0c0f372d4e7e0aacee60
Leastorga <leticia.trancozo@gmail.com>
2023-11-23 13:43:33 -0300

    Listar as pastas a partir de uma pasta informada

---
+++ Directory&DirectoryInfo/Uso/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UsandoDirectoryEDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Users\97525\Desktop\POO\Directory&DirectoryInfo\myfolder";
+
+            try
+            {
+                // *.* qualquer nome de arquivo e 
+                // SearchOption.AllDirectories = Procurar todas as opções em todos os diretórios
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string item in folders)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+
+            }
+
+        }
+    }
+}
