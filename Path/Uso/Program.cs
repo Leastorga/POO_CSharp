68e8b3fcc05abdabd275a0e8f4e5a7b226460326
Leastorga <leticia.trancozo@gmail.com>
2023-11-24 09:48:07 -0300

    Aprendendo a usar a classe Path

---
+++ Path/Uso/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UsoPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Users\97525\Desktop\POO\Directory&DirectoryInfo\myfolder\file01.txt";
+
+            try
+            {
+                Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+                Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+                Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+                Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+                Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+                Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+                Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+                Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
