e9471ba81e06eba858e66b469429eb7a01a31798
Leastorga <leticia.trancozo@gmail.com>
2023-11-30 13:06:51 -0300

    Praticando o uso da interface IComparable

---
+++ Interface/IntrerfaceComparable/Exec/Entities/Employee.cs
@@ -0,0 +1,30 @@
+
+using System.Globalization;
+
+namespace Exec.Entities
+{
+    class Employee : IComparable
+    {
+      public string Name { get; set; }
+      public double Salary { get; set; }
+      
+      public Employee(string csvEmployee){
+        string[] vet = csvEmployee.Split(',');
+        Name = vet[0];
+        Salary = double.Parse(vet[1], CultureInfo.InvariantCulture);
+      }
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if(!(obj is Employee)){
+                throw new ArgumentException("Comparing error: argument is not an employee!");
+            }
+            Employee other = obj as Employee;
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ Interface/IntrerfaceComparable/Exec/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Exec.Entities;
+
+namespace InterfaceComparable {
+    class Program {
+        static void Main(string[] args) {
+
+            string path = @"C:\Users\97525\Desktop\POO\Interface\IntrerfaceComparable\in.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee> ();
+                    while(!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+
+                    list.Sort();  // Para esse função funcionar preciso utilizar a Interface Comparable
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
