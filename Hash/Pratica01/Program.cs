ffc39ef6be1a431eefc0c2c10574338d65691953
Leastorga <leticia.trancozo@gmail.com>
2023-12-04 13:08:26 -0300

    Aprendendo sobre GetHashCode e Equals

---
+++ Hash/Pratica01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Pratica01.Entities;
+
+namespace Pratica01 {
+    class Program {
+        static void Main(string[] args) {
+
+            Client a = new(){Name = "Maria", Email = "maria@gmail.com"};
+            Client b = new(){Name = "Alex", Email = "Alex@gmail.com"};
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+        }
+    }
+}
