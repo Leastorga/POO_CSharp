15f318c6e24cf61f00f541efb7723ff7c844b338
Leastorga <leticia.trancozo@gmail.com>
2023-12-07 16:00:47 -0300

    Desenvolvendo o exercício de fixação

---
+++ Dictionary/Exercicios/Exercicio01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> dictionary = new();
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] votingRecord = sr.ReadLine().Split(',');
+                    string candidate = votingRecord[0];
+                    int votes = int.Parse(votingRecord[1]);
+                    
+                    if (dictionary.ContainsKey(candidate))
+                    {
+                        dictionary[candidate] += votes;
+                    }
+                    else
+                    {
+                        dictionary[candidate] = votes;
+                    }
+                }
+
+                foreach (var item in dictionary)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+
+        }
+
+    }
+}
