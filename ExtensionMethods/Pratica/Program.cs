714cb5d0248429687d68ed39964b90d7ca61a090
Leastorga <leticia.trancozo@gmail.com>
2023-12-07 16:39:42 -0300

    Aprendendo o conceito de extension  methods

---
+++ ExtensionMethods/Pratica/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Pratica.Extension;
+
+namespace Pratica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt = new(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
