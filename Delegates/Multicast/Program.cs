d9008c38488bf7a5dfd17106b448be1be315afa5
Leastorga <leticia.trancozo@gmail.com>
2023-12-11 10:00:00 -0300

    MultiCast delegates

---
+++ Delegates/Multicast/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Multicast.Services;
+
+namespace Multicast {
+    class Program {
+        delegate void BinaryNumericOperation(double n1, double n2);
+        static void Main(string[] args) {
+
+        double a = 10;            
+        double b = 12;
+
+        BinaryNumericOperation op = CalculationService.ShowSum;  
+        op+= CalculationService.ShowMax;    
+        op.Invoke(a,b);      
+
+        }
+    }
+}
